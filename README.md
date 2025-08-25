088bf47a19475dbc6e7d7cb705268b162eb846d5
KanikazeBR <154566908+KanikazeBR@users.noreply.github.com>
2025-08-25 10:50:20 -0300

    Listas Criadas

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Python-ListasDeConvidadosABC
+
+Trablho Feito por Gabriel Falcão, Gabriel Peters e Pietro, 3 C
--- app.py
+++ app.py
@@ -1,1 +1,6 @@
 convidadosDaFesta = ['Ricardo', 'Nicolle', 'Danilo']
+
+statusPresencaa = {}
+
+# Lista de quem tentou entrar
+listaDechegadas = ['Ricardo', 'Nicolle']
